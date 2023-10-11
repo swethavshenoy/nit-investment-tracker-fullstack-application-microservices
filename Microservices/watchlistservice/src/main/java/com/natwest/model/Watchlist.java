75392bb5b6e26694e7b458e1a10989c7bc6f4575
Ubuntu <ubuntu@ip-172-31-3-128.ap-south-1.compute.internal>
2023-10-11 13:18:38 +0000

    update3

---
+++ Microservices/watchlistservice/src/main/java/com/natwest/model/Watchlist.java
@@ -0,0 +1,26 @@
+package com.natwest.model;
+
+import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+
+import java.math.BigDecimal;
+
+
+@Setter
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "watchlist_items")
+public class Watchlist {
+        @MongoId
+        private String id;
+        private String investmentName;
+        private String assetType;
+        private BigDecimal currentPrice;
+        private Long volume;
+        private String industry;
+        private BigDecimal gainLoss;
+}
