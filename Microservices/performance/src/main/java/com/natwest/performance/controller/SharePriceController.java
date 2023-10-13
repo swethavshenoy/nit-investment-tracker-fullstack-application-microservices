f6a320b3a2298649691276d8fa9302b78b91de3b
Swetha <swetha.chandrakefa@natwest.com>
2023-10-13 18:43:51 +0530

    api gateway added

--- Microservices/performance/src/main/java/com/natwest/performance/controller/SharePriceController.java
+++ Microservices/performance/src/main/java/com/natwest/performance/controller/SharePriceController.java
@@ -2,7 +2,6 @@ package com.natwest.performance.controller;
 
 import com.natwest.performance.domain.SharePrice;
 import com.natwest.performance.repository.SharePriceRepository;
-import com.natwest.performance.service.SharePriceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
